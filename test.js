97d81d4c418a2a48ac6af20cc592f6de36dbaef8
Pranav Parakh <31867549+pranavparakh7@users.noreply.github.com>
2018-06-15 10:36:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const button = document.querySelector('#screenshot-button');
 const img = document.querySelector('#screenshot-img');
 const video = document.querySelector('#screenshot-video');
-
 const canvas = document.createElement('canvas');
+var w,h;
 
 button.onclick = video.onclick = function() {
   canvas.width = video.videoWidth;
@@ -10,6 +10,10 @@ button.onclick = video.onclick = function() {
   canvas.getContext('2d').drawImage(video, 0, 0);
   // Other browsers will fall back to image/png
   img.src = canvas.toDataURL('image/webp');
+  img.onload = function(){
+     w = imageElement.naturalWidth;
+     h = imageElement.naturalHeight;
+  }
 };
 const constraints = {
   video: true
@@ -23,3 +27,21 @@ function handleError(error) {
 
 navigator.mediaDevices.getUserMedia(constraints).
     then(handleSuccess).catch(handleError);
+
+function setup() {
+  noLoop();
+}
+
+function draw(keypoints) {
+  createCanvas(w,h);
+  background(0);
+  /*noSmooth();
+
+  // Draw white points
+  stroke(255);
+
+  for(var i in keypoints){
+    strokeWeight(4);
+    point(keypoints[i].position.x,keypoints[i].position.y);
+  }*/
+}
